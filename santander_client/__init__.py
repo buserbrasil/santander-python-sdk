469f574ce530270fc352594781ee2755117639a8
Erle Carrara <carrara.erle@gmail.com>
2025-02-10 11:07:11 -0300

    fix: SantanderApiClient import

--- santander_client/__init__.py
+++ santander_client/__init__.py
@@ -1,4 +1,4 @@
-from santander_client.api_client import SantanderApiClient
+from santander_client.api_client.client import SantanderApiClient
 from santander_client.api_client.client_configuration import (
     SantanderClientConfiguration,
 )
